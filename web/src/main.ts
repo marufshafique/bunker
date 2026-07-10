08f6dec96ae058e5d4a0b532e38916b639ec559f
shm-wtag <maruf.shafique@welldev.io>
2026-07-11 05:52:05 +0600

    add axios plugin

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import axiosPlugin from './plugins/axios'
 
 const app = createApp(App)
 app.use(router)
+app.use(axiosPlugin)
 app.mount('#app')
